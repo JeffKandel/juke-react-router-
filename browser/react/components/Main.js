e764d3b72433d3479f7e59c40f18ddb3aa876e1f
Student <jeff.kandel@gmail.com>
2017-07-05 18:31:20 -0400

    refactor: use Link to connect album images to SingleAlbum pages

--- browser/react/components/Main.js
+++ browser/react/components/Main.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { HashRouter, Route } from 'react-router-dom'
-import axios from 'axios';
 import AllAlbums from './AllAlbums';
 import SingleAlbum from './SingleAlbum';
 import Sidebar from './Sidebar';
@@ -10,40 +9,23 @@ export default class Main extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      selectedAlbum: {}
-    };
-    this.selectAlbum = this.selectAlbum.bind(this);
-    this.deselectAlbum = this.deselectAlbum.bind(this);
-  }
-
-  selectAlbum(albumId) {
-    axios.get(`/api/albums/${albumId}`)
-      .then(res => res.data)
-      .then(album => this.setState({
-        selectedAlbum: album
-      }));
-  }
-
-  deselectAlbum() {
-    this.setState({ selectedAlbum: {} });
   }
 
   render() {
     return (
-      <div id="main" className="container-fluid">
-        <div className="col-xs-2">
-          <Sidebar deselectAlbum={this.deselectAlbum} />
+      <HashRouter>
+        <div id="main" className="container-fluid">
+            <div className="col-xs-2">
+              <Sidebar deselectAlbum={this.deselectAlbum} />
+            </div>
+            <div className="col-xs-10">
+              <Route exact path="/" component={AllAlbums} />
+              <Route exact path="/albums" component={AllAlbums} />
+              <Route path="/albums/:albumId" component={SingleAlbum} />
+            </div>
+          <Player />
         </div>
-        <HashRouter>
-          <div className="col-xs-10">
-            <Route exact path="/" component={AllAlbums} />
-            <Route exact path="/albums" component={AllAlbums} />
-            <Route path="/albums/:albumId" component={SingleAlbum} />
-          </div>
-        </HashRouter>
-        <Player />
-      </div>
+      </HashRouter>
     );
   }
 }
--- browser/react/components/SingleAlbum.js
+++ browser/react/components/SingleAlbum.js
@@ -2,21 +2,6 @@ import React, { Component } from 'react';
 import Songs from '../components/Songs';
 import axios from 'axios';
 
-const fakeAlbum = {
-  name: 'Yellow Submarine',
-  id: 2,
-  imageUrl: 'http://fillmurray.com/300/300',
-  songs: [{
-    id: 4,
-    name: 'London Calling',
-    artists: [
-      { name: 'Bill' }
-    ],
-    genre: 'Punk',
-    audioUrl: 'https://learndotresources.s3.amazonaws.com/workshop/5616dbe5a561920300b10cd7/Dexter_Britain_-_03_-_The_Stars_Are_Out_Interlude.mp3'
-  }]
-}
-
 export default class SingleAlbum extends Component {
   constructor () {
     super();
@@ -25,16 +10,14 @@ export default class SingleAlbum extends Component {
     };
   }
   componentDidMount () {
-    axios.get('/api/albums/' + this.props.match.params.albumId)
+    axios.get(`/api/albums/${this.props.match.params.albumId}`)
       .then(albumRes => {
-        console.log('album fetched: ' + albumRes);
         return albumRes.data
       }).then(album => {
         this.setState({album})
       });
   }
   render() {
-    console.log("params", this.props.match.params)
     const album = this.state.album;
 
     return (
